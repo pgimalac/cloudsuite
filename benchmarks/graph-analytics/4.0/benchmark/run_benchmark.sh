7c0c4c781d4c8bb892c4e4217ef0e4816eb60663
Pierre Gimalac <pierre.gimalac@gmail.com>
2021-12-07 09:37:22 +0100

    extract traces graph-analytics

--- benchmarks/graph-analytics/4.0/benchmark/run_benchmark.sh
+++ benchmarks/graph-analytics/4.0/benchmark/run_benchmark.sh
@@ -9,7 +9,9 @@ Usage: graph-analytics [SPARK_OPTIONS]
   SPARK_OPTIONS are passed on to spark-submit.
 EOS
 
-${SPARK_HOME}/bin/spark-submit "$@" \
+strace -f -ttt -T -o strace.out ${SPARK_HOME}/bin/spark-submit "$@" \
     --class "GraphAnalytics" \
     ${BENCHMARK_DIR}/graph-analytics_2.11-1.0.jar \
     -app="${WORKLOAD_NAME}"
+echo "DONE"
+sleep 100000
